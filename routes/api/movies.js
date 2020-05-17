aea92a616b07d30b9b71ec528ba4a8a653ff689c
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-05-17 23:40:43 +0100

    added get by id route

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -7,12 +7,20 @@ const router = express.Router();
 const baseURL = config.get('TMDb.baseURL');
 const api_key = config.get('TMDb.api_key');
 
+// @route GET movies
 router.get('/', (req, res) => {
   const url = `/movie/531428/recommendations?api_key=${api_key}&language=en-US&page=1`;
   axios.get(baseURL + url)
     .then(result => res.send(result.data))
     .catch(err => res.send(err));
-  // res.send(baseURL + url);
+})
+
+// @route GET movie by ID
+router.get('/:id', (req, res) => {
+  const url = `/movie/${req.params.id}?api_key=${api_key}&language=en-US`;
+  axios.get(baseURL + url)
+    .then(result => res.send(result.data))
+    .catch(err => res.send(err));
 })
 
 module.exports = router;
