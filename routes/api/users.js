f5f866debd95188dceb1aadbe31432436189a15c
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-07-21 22:05:20 +0100

    Added validation to users route.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,23 +1,46 @@
-const _ = require('lodash');
 const express = require('express');
+const _ = require('lodash');
+const Joi = require('@hapi/joi');
 
 const router = express.Router();
 
 const User = require('../../models/user');
 
-// @route POST api/users
+// @route   POST api/users
+// @desc    Register users
+// @access  Public
 router.post('/', async (req, res) => {
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send({ msg: error.details[0].message });
+
   let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send('User already registered.');
+  if (user) return res.status(400).send({ msg: 'User already registered.' });
 
   user = new User(_.pick(req.body, ['username', 'email', 'password']));
-  
+
   user.password = await user.generateHash(user.password);
 
-  await user.save();
+  try {
+    await user.save();
+  } catch (ex) {
+    if(ex.code === 11000) ex.message = 'Username already exists.';
+    return res.status(400).send({ msg: ex.message });
+  }
 
   const token = user.generateAuthToken();
-  res.header('x-auth-token', token).send(_.pick(user, ['name', ['email']]));
+  return res.header('x-auth-token', token).send(_.pick(user, ['username', ['email']]));
 })
 
 module.exports = router;
+
+
+// Validate register inputs
+function validateUser(user) {
+  const schema = Joi.object().keys({
+    email: Joi.string().email({ minDomainSegments: 2 }).required(),
+    username: Joi.string().alphanum().min(3).max(32).required(),
+    password: Joi.string().min(8).max(32).required(),
+  })
+
+  return schema.validate(user);
+}
