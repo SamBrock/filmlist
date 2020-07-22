0d09cac5a66df336f95e24d7affc597914cdc394
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-07-22 23:12:37 +0100

    Updated register modal

--- client/src/components/auth/RegisterModal.js
+++ client/src/components/auth/RegisterModal.js
@@ -1,17 +1,24 @@
-import React, { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { registerUser, getIsAuthenticated } from '../../store/auth'
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { registerUser, getIsAuthenticated } from '../../store/auth';
+import { clearErrors } from '../../store/error';
 import { getRegisterError } from '../../store/error';
-import { Link } from 'react-router-dom';
 
 export default function RegisterModal(props) {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  const registerError = useSelector(getRegisterError);
+  const [modal, setModal] = useState(false);
 
   const dispatch = useDispatch();
+
+  const registerError = useSelector(getRegisterError);
+  
+  const isAuthenticated = useSelector(getIsAuthenticated);
+  useEffect(() => {
+      if (isAuthenticated) setModal(false);
+  }, [isAuthenticated, modal])
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,17 +29,29 @@ export default function RegisterModal(props) {
     if (document.getElementById('password').value === document.getElementById('repeatPassword').value) return true; else return false;
   }
 
+  const closeModal = () => {
+    setModal(false);
+    dispatch(clearErrors());
+  }
+
   return (
-    <div className="register-modal-container" style={props.isOpen ? { display:'block'} : {display : 'none'}} >
-      <div className="register-modal center">
-        <h2>Register</h2>
-        <input type="text" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}></input>
-        <input type="text" name="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)}></input>
-        <input id="password" type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}></input>
-        <input id="repeatPassword" type="password" name="repeat-password" placeholder="Confirm password" onChange={(e) => setPassword(e.target.value)}></input>
-        <input className="btn-primary" type="submit" value="Register" onClick={(e) => handleSubmit(e)} />
-        <span>{registerError}</span>
+    <React.Fragment >
+      <div className="nav-link">
+        <a onClick={() => setModal(true)}>Register</a>
       </div>
-    </div>
+
+      <div className="register-modal-container" style={modal ? { display: 'block' } : { display: 'none' }} >
+        <div className="register-modal center">
+          <a onClick={closeModal}>Close</a>
+          <h2>Register</h2>
+          <input type="text" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}></input>
+          <input type="text" name="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)}></input>
+          <input id="password" type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}></input>
+          <input id="repeatPassword" type="password" name="repeat-password" placeholder="Confirm password" onChange={(e) => setPassword(e.target.value)}></input>
+          <input className="btn-primary" type="submit" value="Register" onClick={(e) => handleSubmit(e)} />
+          <span>{registerError}</span>
+        </div>
+      </div>
+    </React.Fragment>
   )
 }
