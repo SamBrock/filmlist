746c5fb55c4b4076e34176252fd274688d000b33
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-07-22 23:13:28 +0100

    Added register route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,28 +1,72 @@
-const _ = require('lodash');
 const express = require('express');
+const _ = require('lodash');
+const Joi = require('@hapi/joi');
 
 const router = express.Router();
 
 const User = require('../../models/user');
-
 const auth = require('../../middleware/auth');
 
-// @route POST api/auth
-router.post('/', async (req, res) => {
-  let user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send('Invalid email or password.');
+// @route   POST api/users
+// @desc    Register users
+// @access  Public
+router.post('/register', async (req, res) => {
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send({ id: 'REGISTER_ERROR', msg: error.details[0].message });
 
-  const validPassword = await user.validPassword(req.body.password);
-  if (!validPassword) return res.status(400).send('Invalid email or password.');
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send({ type: 'REGISTER_ERROR', msg: 'User already registered.' });
+
+  user = new User(_.pick(req.body, ['username', 'email', 'password']));
+
+  user.password = await user.generateHash(user.password);
+
+  try {
+    await user.save();
+  } catch (ex) {
+    if (ex.code === 11000) ex.message = 'Username already exists.';
+    return res.status(400).send({ msg: ex.message });
+  }
 
   const token = user.generateAuthToken();
-  res.send(token);
+  // return res.header('x-auth-token', token).send(_.pick(user, ['username', ['email']]));
+  return res.status(200).send({ token, user: { id: user._id, username: user.username, email: user.email } })
 })
 
-router.get('/user', auth, (req, res) => {
-  User.findById(req.user.id)
-  .select('-password')
-  .then(user => res.json(user));
+// @route POST api/auth
+router.post('/login', async (req, res) => {
+  console.log(req.body);
+  if (!req.body.email || !req.body.password) return res.status(400).send({ id: 'LOGIN_ERROR', msg: 'Please enter all fields.' });
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send({ id: 'LOGIN_ERROR', msg: 'Invalid email or password.' });
+
+  const validPassword = await user.validPassword(req.body.password);
+  if (!validPassword) return res.status(400).send({ id: 'LOGIN_ERROR', msg: 'Invalid email or password.' });
+
+  const token = user.generateAuthToken();
+  res.send({ token, user: { id: user._id, username: user.username, email: user.email } });
+})
+
+router.get('/user', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    if (!user) throw Error('User Does not exist');
+    res.send(user);
+  } catch (err) {
+    res.status(400).send({ id: 'USER_ERROR', msg: err.message });
+  }
 })
 
 module.exports = router;
+
+// Validate register inputs
+function validateUser(user) {
+  const schema = Joi.object().keys({
+    email: Joi.string().email({ minDomainSegments: 2 }).required(),
+    username: Joi.string().alphanum().min(3).max(32).required(),
+    password: Joi.string().min(8).max(32).required(),
+  })
+
+  return schema.validate(user);
+}
