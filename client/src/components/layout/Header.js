151294c66168d5435d338cd12c99d32925e85364
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-07-22 23:12:48 +0100

    Added modal links to navbar

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,23 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, Fragment } from 'react'
 import Logo from '../../images/f-logo.png'
 import Search from '../../images/search.png'
 import { Link } from 'react-router-dom'
 import RegisterModal from '../auth/RegisterModal'
+import LoginModal from '../auth/LoginModal'
+import Logout from '../auth/Logout'
+import { useSelector } from 'react-redux'
+import { getIsAuthenticated, getUser } from '../../store/auth'
 
 export default function Header() {
-  const [registerModal, setRegisterModal] = useState(false);
+  const isAuthenticated = useSelector(getIsAuthenticated);
+  const user = useSelector(getUser);
 
-  const openModal = () => setRegisterModal(true);
-  const closeModal = () => setRegisterModal(false);
+  const authLinks = (
+    <Fragment>
+      <Logout />
+    </Fragment>
+  )
+
+  const guestLinks = (
+    <Fragment>
+      <RegisterModal />
+      <LoginModal />
+    </Fragment>
+  )
 
   return (
     <header>
       <Link to="/"><img src={Logo} className="logo" alt="Logo" /></Link>
       <img src={Search} className="search-btn" alt="Search button" />
-      <div className="nav-link">
-        <a onClick={openModal}>Register</a>
-        <RegisterModal isOpen={registerModal} />
-      </div>
+      {isAuthenticated ? authLinks : guestLinks}
     </header>
   )
 }
