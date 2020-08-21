7ae62492a20f9144425c6d64b6f603d8df170444
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-08-21 21:51:35 +0100

    Added load more button.

--- client/src/pages/MoviesPage.js
+++ client/src/pages/MoviesPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Fragment } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { loadMovies, getMovies } from '../store/movies';
 import MovieItem from '../components/MovieItem'
@@ -9,7 +9,9 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 export default function Movies() {
   const [pageNumber, setPageNumber] = useState(1);
   const [limit, setLimit] = useState(20);
-  
+  const [hasMore, setHasMore] = useState(true);
+  const [count, setCount] = useState(0);
+
   const dispatch = useDispatch();
 
   const isAuthenticated = useSelector(getIsAuthenticated);
@@ -18,6 +20,15 @@ export default function Movies() {
   }, [isAuthenticated, pageNumber])
 
   const movies = useSelector(getMovies);
+
+  useEffect(() => {
+    setCount(count => count + movies.length);
+
+    if (count > 60) {
+      setHasMore(false);
+      setCount(0);
+    }
+  }, [movies])
 
   const isLoading = useSelector(state => state.entities.movies.loading);
   if (isLoading) {
@@ -28,12 +39,17 @@ export default function Movies() {
   document.title = `FILMLIST`
   dispatch(complete());
   return (
-    <InfiniteScroll dataLength={movies.length} next={() => setPageNumber(page => page + 1)} hasMore={true} loader={<h4>Loading...</h4>} endMessage={<p style={{ textAlign: 'center' }}> <b>Yay! You have seen it all</b> </p>} >
-      <div className="movies-container movies" data-router-view="movie">
-        {movies.map((movie) => (
-          <MovieItem key={movie.id} movie={movie} page="movies" />
-        ))}
+    <Fragment>
+      <InfiniteScroll dataLength={movies.length} next={() => setPageNumber(page => page + 1)} hasMore={hasMore} >
+        <div className="movies-container movies" data-router-view="movie">
+          {movies.map((movie) => (
+            <MovieItem key={movie.id} movie={movie} page="movies" />
+          ))}
+        </div>
+      </InfiniteScroll>
+      <div className="load-more-container" style={hasMore ? { display: 'none' } : {}}>
+        <a className="load-more-btn btn-primary" onClick={() => setHasMore(true)}>Load more films</a>
       </div>
-    </InfiniteScroll>
-  )
+    </Fragment>
+  ) 
 }
