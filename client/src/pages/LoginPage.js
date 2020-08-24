5eadebaf62d25586fb366bcf94f187b276856ec3
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-08-24 22:53:52 +0100

    Updated pages.

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -4,13 +4,22 @@ import { getLoginError } from '../store/error';
 import { loginUser, getIsAuthenticated } from '../store/auth';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import BackdropImg from '../components/layout/BackdropImg';
+import { motion } from 'framer-motion';
+import { loginBackdrops } from '../components/layout/backdrops'
+import Footer from '../components/layout/Footer';
 
 export default function LoginPage() {
+  const [backdropsIndex, setBackdropsIndex] = useState(0);
+  useEffect(() => {
+    // Select random backdrop
+    setBackdropsIndex(Math.floor(Math.random() * loginBackdrops.length))
+  }, [])
+
   const dispatch = useDispatch();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [modal, setModal] = useState(false);
 
   const loginError = useSelector(getLoginError);
   const isAuthenticated = useSelector(getIsAuthenticated);
@@ -25,18 +34,27 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="container">
-      <div className="col-backdrop">
-        <img src={"https://image.tmdb.org/t/p/original//spoZUN4X1KiOc5S0plOyGAXLNtb.jpg"}></img>
+    <motion.div exit={{ opacity: 1 }} className="grid-page-container">
+      <BackdropImg backdropPath={loginBackdrops[backdropsIndex].backdropPath} />
+      <div className="grid-2-col-backdrop ">
+        <div id="backdrop-placeholder"></div>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="content-col form-container">
+          <div className="form">
+            <h1>Log in</h1>
+            <span className="form-error">{loginError}</span>
+            <input className="form-control fc-email" type="text" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}></input>
+            <input className="form-control fc-lock" id="password" type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}></input>
+            <input className="btn-primary" type="submit" value="Log in" onClick={(e) => handleSubmit(e)} />
+            <span className="register-now text-center">Don't have an account yet? <Link to='/register'>Register now</Link></span>
+          </div>
+          <Footer />
+        </motion.div>
       </div>
-      <div className="col-hw c login-container">
-        <h1>Log in</h1>
-        <span className="form-error">{loginError}</span>
-        <input className="form-control fc-email" type="text" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}></input>
-        <input className="form-control fc-lock" id="password" type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}></input>
-        <input className="btn-primary" type="submit" value="Log in" onClick={(e) => handleSubmit(e)} />
-        <span className="register-now text-center">Don't have an account yet? <Link to='/register'>Register now</Link></span>
+      <div className="backdrop-info">
+        <div><span className="backdrop-movie-title">{loginBackdrops[backdropsIndex].movie}</span>, {loginBackdrops[backdropsIndex].year}</div>
+        <div>{loginBackdrops[backdropsIndex].director}</div>
       </div>
-    </div>
+
+    </motion.div>
   )
 }
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,57 +1,67 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getRegisterError, clearErrors } from '../store/error';
-import { loginUser, getIsAuthenticated, registerUser } from '../store/auth';
+import { getRegisterError } from '../store/error';
+import { getIsAuthenticated, registerUser } from '../store/auth';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import BackdropImg from '../components/layout/BackdropImg';
+import { motion } from 'framer-motion';
+import { registerBackdrops } from '../components/layout/backdrops'
+import Footer from '../components/layout/Footer';
 
 export default function RegisterPage() {
+  const [backdropsIndex, setBackdropsIndex] = useState(0);
+  useEffect(() => {
+    // Select random backdrop
+    setBackdropsIndex(Math.floor(Math.random() * registerBackdrops.length))
+  }, [])
+
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [modal, setModal] = useState(false);
 
   const dispatch = useDispatch();
 
-  const registerError = useSelector(getRegisterError);
+  let registerError = useSelector(getRegisterError);
 
   const isAuthenticated = useSelector(getIsAuthenticated);
   useEffect(() => {
-    if (isAuthenticated) setModal(false);
-  }, [isAuthenticated, modal])
+    if (isAuthenticated) return window.location.replace('http://localhost:3000/');
+  }, [isAuthenticated])
 
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(registerUser(username, email, password));
+    verifyPassword ? dispatch(registerUser(username, email, password)) : registerError = 'Passwords do not match'
   }
 
   const verifyPassword = () => {
     if (document.getElementById('password').value === document.getElementById('repeatPassword').value) return true; else return false;
   }
 
-  const closeModal = () => {
-    setModal(false);
-    dispatch(clearErrors());
-  }
-
   return (
-    <div className="container">
-      <div className="col-backdrop">
-        <img src={"https://image.tmdb.org/t/p/original/czrTZnZgSwtIofk0UYrRMicVgHB.jpg"}></img>
+    <motion.div exit={{ opacity: 1 }} className="grid-page-container">
+      <BackdropImg backdropPath={registerBackdrops[backdropsIndex].backdropPath} />
+      <div className="grid-2-col-backdrop ">
+        <div id="backdrop-placeholder"></div>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="content-col form-container">
+          <div className="form">
+            <h1>Register</h1>
+            <span className="form-error">{registerError}</span>
+            <input className="form-control fc-email" type="text" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}></input>
+            <input className="form-control fc-user" type="text" name="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)}></input>
+            <input className="form-control fc-lock" id="password" type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}></input>
+            <input className="form-control fc-lock" id="repeatPassword" type="password" name="repeat-password" placeholder="Confirm password" onChange={(e) => setPassword(e.target.value)}></input>
+            <input className="btn-primary" type="submit" value="Register" onClick={(e) => handleSubmit(e)} />
+            <span className="register-now text-center">Already have an account? <Link to='/login'>Log in</Link></span>
+          </div>
+          <Footer />
+        </motion.div>
       </div>
-      <div className="col-hw c login-container">
-        <h1>Register</h1>
-        <span className="form-error">{registerError}</span>
-        <input className="form-control fc-email" type="text" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}></input>
-        <input className="form-control fc-user" type="text" name="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)}></input>
-        <input className="form-control fc-lock" id="password" type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}></input>
-        <input className="form-control fc-lock" id="repeatPassword" type="password" name="repeat-password" placeholder="Confirm password" onChange={(e) => setPassword(e.target.value)}></input>
-        <input className="btn-primary" type="submit" value="Register" onClick={(e) => handleSubmit(e)} />
-        <span className="register-now text-center">Already have an account? <Link to='/login'>Log in</Link></span>
-
-        
+      <div className="backdrop-info">
+        <div><span className="backdrop-movie-title">{registerBackdrops[backdropsIndex].movie}</span>, {registerBackdrops[backdropsIndex].year}</div>
+        <div>{registerBackdrops[backdropsIndex].director}</div>
       </div>
-    </div>
+    </motion.div>
   )
 }
