c98921c8705fac971aa1c64fca50c3005863e139
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-08-24 22:53:14 +0100

    Added dropdown to logout.

--- client/src/components/ProfileButton.js
+++ client/src/components/ProfileButton.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState, Fragment } from 'react'
+import { logoutUser } from '../store/auth';
+import { useDispatch } from 'react-redux';
+
 
 export default function ProfileButton({ username }) {
+  const [hover, setHover] = useState(true);
   const usernameLetter = username.toUpperCase().charAt(0);
 
+  const dispatch = useDispatch();
+
   return (
-    <div className="profile-btn">
-      {usernameLetter}
-    </div>
+    <Fragment>
+      <div className="profile-btn-container" onMouseOver={() => setHover(true)} onMouseLeave={() => setTimeout(() => setHover(false), 500)}>
+        <div className="profile-btn">
+          {usernameLetter}
+        </div>
+        <div className="profile-dropdown" style={!hover ? { display: 'none' } : {}}>
+          <ul>
+            <li><a onClick={() => dispatch(logoutUser())}>Logout</a></li>
+          </ul>
+        </div>
+      </div>
+    </Fragment>
   )
 }
