cbcf4e5ecbd3c3f8f5d176d8225bc50deacb7b43
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-08-24 22:52:19 +0100

    Added component for backdrop images.

---
+++ client/src/components/layout/BackdropImg.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react'
+import { useWindowSize } from '../../hooks/useWindowSize';
+import { motion } from 'framer-motion'
+import ProgressiveImage from 'react-progressive-image';
+
+const transition = { ease: [0.43, 0.13, 0.23, 0.96] }
+
+export default function BackdropImg({ backdropPath }) {
+  const [backdropWidth, setBackdropWidth] = useState(0);
+  const [width, height] = useWindowSize();
+
+  useEffect(() => {
+    const element = document.getElementById('backdrop-placeholder');
+    if (element) setBackdropWidth(element.getBoundingClientRect().width);
+  }, [width, height])
+
+  return (
+    <div className="backdrop-container l-grid-col" style={{ width: `${backdropWidth}px` }}>
+      <ProgressiveImage src={"https://image.tmdb.org/t/p/original" + backdropPath} placeholder={"https://image.tmdb.org/t/p/w780" + backdropPath}>
+        {src => <motion.img initial={{ scale: 1.1, opacity: .3 }} animate={{ scale: 1, opacity: 1  }} exit={{ scale: 1.1, opacity: .3 }} transition={transition} src={src} alt={`movie backdrop`} />}
+      </ProgressiveImage>
+    </div >
+  )
+}
