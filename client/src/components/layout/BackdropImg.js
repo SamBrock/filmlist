23095dc93678eb835a9a134457172e75cd40176c
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-08-28 17:22:28 +0100

    Updated scroll function to own hook.

--- client/src/components/layout/BackdropImg.js
+++ client/src/components/layout/BackdropImg.js
@@ -1,23 +1,41 @@
 import React, { useState, useEffect } from 'react'
-import { useWindowSize } from '../../hooks/window-hooks';
+import { useWindowSize, usePageYOffset } from '../../hooks/window-hooks';
 import { motion } from 'framer-motion'
 import ProgressiveImage from 'react-progressive-image';
 
 const transition = { ease: [0.43, 0.13, 0.23, 0.96] }
 
 export default function BackdropImg({ backdropPath }) {
+  const pageYOffset = usePageYOffset();
   const [backdropWidth, setBackdropWidth] = useState(0);
+  const [backdropHeight, setBackdropHeight] = useState(0);
   const [width, height] = useWindowSize();
+
+  useEffect(() => {
+    if (pageYOffset === 0) return setBackdropHeight(height);
+    setBackdropHeight(height - pageYOffset);
+  })
 
   useEffect(() => {
     const element = document.getElementById('backdrop-placeholder');
     if (element) setBackdropWidth(element.getBoundingClientRect().width);
   }, [width, height])
 
+  // For tablet and smaller resolutions
+  if (width <= 768) {
+    return (
+      <div className={`backdrop-container l-grid-col`} style={{ height: backdropHeight }}>
+        <ProgressiveImage src={"https://image.tmdb.org/t/p/original" + backdropPath} placeholder={"https://image.tmdb.org/t/p/w780" + backdropPath}>
+          {src => <motion.img initial={{ scale: 1.1, opacity: .3 }} animate={{ scale: 1, opacity: 1 }} exit={{ scale: 1.1, opacity: .3 }} transition={transition} src={src} alt={`movie backdrop`} style={{ marginTop: pageYOffset }} />}
+        </ProgressiveImage>
+      </div >
+    )
+  }
+
   return (
     <div className="backdrop-container l-grid-col" style={{ width: `${backdropWidth}px` }}>
       <ProgressiveImage src={"https://image.tmdb.org/t/p/original" + backdropPath} placeholder={"https://image.tmdb.org/t/p/w780" + backdropPath}>
-        {src => <motion.img initial={{ scale: 1.1, opacity: .3 }} animate={{ scale: 1, opacity: 1  }} exit={{ scale: 1.1, opacity: .3 }} transition={transition} src={src} alt={`movie backdrop`} />}
+        {src => <motion.img initial={{ scale: 1.1, opacity: .3 }} animate={{ scale: 1, opacity: 1 }} exit={{ scale: 1.1, opacity: .3 }} transition={transition} src={src} alt={`movie backdrop`} />}
       </ProgressiveImage>
     </div >
   )
--- client/src/hooks/window-hooks.js
+++ client/src/hooks/window-hooks.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useLayoutEffect, useState, useEffect } from 'react';
 
 export function useWindowSize() {
   const [size, setSize] = useState([0, 0]);
@@ -13,15 +13,38 @@ export function useWindowSize() {
   return size;
 }
 
-// export function useWindowSize() {
-//   const [scroll, setScroll] = useState([0, 0]);
-//   useLayoutEffect(() => {
-//     function updateSize() {
-//       setSize([window.innerWidth, window.innerHeight]);
-//     }
-//     window.addEventListener('resize', updateSize);
-//     updateSize();
-//     return () => window.removeEventListener('resize', updateSize);
-//   }, []);
-//   return size;
-// }
+export function useHasScrolled() {
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  const handleScroll = () => {
+    setHasScrolled(window.pageYOffset != 0) 
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return hasScrolled;
+}
+
+export function usePageYOffset() {
+  const [pageYOffset, setPageYOffset] = useState(0);
+  
+  const handleScroll = () => {
+    setPageYOffset(window.pageYOffset);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return pageYOffset;
+}
