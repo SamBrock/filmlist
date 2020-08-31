57d35ded5ef20994e1856754bff4d5920e9e2b92
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-08-31 22:47:22 +0100

    Link to portfolio.

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -4,6 +4,7 @@ import GitHub from '../../images/social/github.svg'
 import Dribbble from '../../images/social/dribbble.svg'
 import Twitter from '../../images/social/twitter.svg'
 import LinkedIn from '../../images/social/linkedin.svg'
+import MyLogo from '../../images/s-logo-full.svg'
 import moment from 'moment';
 
 export default function Footer() {
@@ -16,6 +17,7 @@ export default function Footer() {
           <a href="https://www.linkedin.com/in/sam-brocklehurst/" className="social-link"><img src={LinkedIn} /></a>
           <a href="https://dribbble.com/sambrock" className="social-link"><img src={Dribbble} /></a>
           <a href="https://twitter.com/SxmBrock" className="social-link"><img src={Twitter} /></a>
+          <a href="https://sambrock.net" className="social-link"><img src={MyLogo} /></a>
         </div>
       </div>
       <div className="footer-copyright">&copy; {moment().year()} <a href="https://sambrock.net">sambrock.net</a></div>
