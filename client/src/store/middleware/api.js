9497e5602161e01b327e908c93f486cb6712ff20
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-08-31 22:48:29 +0100

    Added onFail action type.

--- client/src/store/middleware/api.js
+++ client/src/store/middleware/api.js
@@ -4,7 +4,7 @@ import * as actions from '../api';
 const api = ({ dispatch }) => next => async action => {
   if (action.type !== actions.apiRequest.type) return next(action);
 
-  const { url, method, data, onStart, onSuccess, onError, headers } = action.payload;
+  const { url, method, data, onStart, onSuccess, onError, onFail, headers } = action.payload;
 
   if (onStart) dispatch({ type: onStart });
   next(action);
@@ -16,7 +16,8 @@ const api = ({ dispatch }) => next => async action => {
     if (onSuccess) dispatch({ type: onSuccess, payload: response.data })
   } catch (err) {
     dispatch(actions.apiRequestFailed());
-    if (onError) dispatch({ type: onError, payload: { id: err.response.data.id, status: err.response.status, msg: err.response.data.msg } })
+    if (onError) dispatch({ type: onError, payload: { id: err.response.data.id, status: err.response.status, msg: err.response.data.msg } });
+    if (onFail) dispatch({ type: onFail, payload: { id: err.response.data.id, status: err.response.status, msg: err.response.data.msg } });
   }
 }
 
