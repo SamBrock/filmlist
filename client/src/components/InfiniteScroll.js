cdabe4655180a95ca270799e2fde826bc1e2f2fb
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-05-20 16:23:52 +0100

    Fixed page glitching after loading.

--- client/src/components/InfiniteScroll.js
+++ client/src/components/InfiniteScroll.js
@@ -1,9 +1,16 @@
-import React, { useEffect, useRef, useState } from 'react'
-import ClipLoader from 'react-spinners/ClipLoader';
+import React, { useEffect, useRef, useState } from 'react';
+import { css } from 'styled-components';
+import BarLoader from 'react-spinners/BarLoader';
+
+const BarLoaderStyles = css`
+    background-color: var(--opacity-1);
+    .css-sqdys8 { background-color: var(--white); }
+    .css-ps9j9u { background-color: var(--white); }
+`;
 
 export default function InfiniteScroll({ children, loadMore, loading }) {
   const [active, setActive] = useState(false);
-  
+
   const loader = useRef(null);
 
   useEffect(() => {
@@ -22,13 +29,13 @@ export default function InfiniteScroll({ children, loadMore, loading }) {
     if (target.isIntersecting) loadMore();
   }
 
-  console.log(loading);
-
   return (
     <div>
       {children}
-      <div className="mt-6 mb-3" ref={loader} />
-      {loading && (<div className="loading mb-6 flex w-full border justify-center" ><ClipLoader color='#fffff' size={35} /></div>)}
+      <div className="flex items-center py-6">
+        <div className="h-5" ref={loader} />
+        {loading && (<div className="loading flex w-full border justify-center" ><BarLoader loading={loading} css={BarLoaderStyles} size={25} /></div>)}
+      </div>
     </div>
   )
 }
